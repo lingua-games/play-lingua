b507ab5803e5c44703fa783c07f9331bf51aeba8
Arash Bahreini <arash@omniaretail.com>
2020-10-05 17:13:55 +0200

    Design the first ugly game :D

--- client/src/app/main/game/falling-stars/falling-stars.component.ts
+++ client/src/app/main/game/falling-stars/falling-stars.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GamesService} from '../../../service/games.service';
 
 @Component({
   selector: 'app-falling-stars',
@@ -6,10 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./falling-stars.component.scss']
 })
 export class FallingStarsComponent implements OnInit {
+  words: any[];
+  currentWord: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private gamesService: GamesService) {
   }
 
+  ngOnInit(): void {
+    this.getGameWords();
+  }
+
+  getGameWords(): void {
+    this.words = [];
+    this.gamesService.getGameWords().subscribe((res: string[]) => {
+      res.forEach((element, index: number) => {
+        this.words.push({
+          value: element,
+          style: {left: `${index * 10 + 10}%`, top: '-10%'},
+          typingWord: ''
+        });
+        this.startWord(this.words[0]);
+      });
+    });
+  }
+
+  startWord(word): void {
+    this.currentWord = word;
+    const presentingWord = setInterval(() => {
+      console.log(word);
+      const top = parseInt(word.style.top, null);
+      if (word.typingWord.toLowerCase() === word.value.toLowerCase()) {
+        word.answered = true;
+        clearInterval(presentingWord);
+        this.endWord(word);
+
+      } else {
+        if (top < 100) {
+          word.style.top = (top + 1) + '%';
+        } else {
+          clearInterval(presentingWord);
+          this.endWord(word);
+        }
+      }
+    }, 500);
+  }
+
+  endWord(word): void {
+    word.terminate = true;
+    const index = this.words.indexOf(word);
+    if (this.words.length !== index - 1 && this.words[index + 1]) {
+      this.startWord(this.words[index + 1]);
+    }
+  }
+
+  // checkWord(event: KeyboardEvent): void {
+  //   if (this.typingWord.toLowerCase() === this.currentWord.value.toLowerCase()) {
+  //     clearInterval(this.presentingWord);
+  //     this.endWord(this.currentWord);
+  //     this.typingWord = '';
+  //   }
+  // }
 }
+
--- client/src/app/main/main/main.module.ts
+++ client/src/app/main/main/main.module.ts
@@ -2,12 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MainComponent} from './main.component';
 import {MainRoutingModule} from './main-routing.module';
+import {FallingStarsComponent} from '../game/falling-stars/falling-stars.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, FallingStarsComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormsModule
   ]
 })
 export class MainModule {
