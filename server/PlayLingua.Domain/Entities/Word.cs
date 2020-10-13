57f254a088f788c18305b4a58379bfcdf2ce5cf1
Senem Unal <senem@omniaretail.com>
2020-10-13 09:54:37 +0200

    Add Book, Chapter, Word entities

---
+++ server/PlayLingua.Domain/Entities/Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PlayLingua.Domain.Entities
+{
+    public class Word
+    {
+        public Word(Guid wordId, LangCode baseLanguage, string baseWord, LangCode targetLanguage, string translate)
+        {
+            WordId         = wordId;
+            BaseLanguage   = baseLanguage;
+            BaseWord       = baseWord;
+            TargetLanguage = targetLanguage;
+            Translate      = translate;
+        }
+
+        public Guid WordId { get; }
+        public LangCode BaseLanguage { get; }
+        public string BaseWord { get; }
+        public LangCode TargetLanguage { get; }
+        public string Translate { get; }
+    }
+
+    public enum LangCode
+    {
+        None = 0,
+        EN   = 1,
+        NL   = 2
+    }
+}
