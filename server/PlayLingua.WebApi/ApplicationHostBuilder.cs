aa41f7b02a6b584de3fec6c48eb2473bb4ddece0
Senem Unal <senem@omniaretail.com>
2020-10-13 10:00:04 +0200

    Add configuration for InMemory repositories

--- server/PlayLingua.WebApi/ApplicationHostBuilder.cs
+++ server/PlayLingua.WebApi/ApplicationHostBuilder.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PlayLingua.Adapter.InMemoryDb;
 using PlayLingua.WebApi.Configuration;
 
 namespace PlayLingua.WebApi
@@ -32,6 +33,8 @@ namespace PlayLingua.WebApi
             services.AddRouting(options => { options.LowercaseUrls = true; });
 
             services.AddSwagger();
+
+            services.AddInMemoryRepository();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
