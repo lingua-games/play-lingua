deab517f8c564dc95cc869c3ec71d2d7fc8e20cd
Senem Unal <senem@omniaretail.com>
2020-10-13 10:04:45 +0200

    Add controller for books and chapters

---
+++ server/PlayLingua.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayLingua.Domain.Entities;
+using PlayLingua.Domain.Ports;
+using System;
+using System.Collections.Generic;
+
+namespace PlayLingua.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IChapterRepository _chapterRepository;
+
+        public BooksController(IBookRepository bookRepository, IChapterRepository chapterRepository)
+        {
+            _bookRepository    = bookRepository;
+            _chapterRepository = chapterRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            var books = _bookRepository.GetBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("{bookId}/chapters")]
+        public ActionResult<List<Chapter>> GetChapters(Guid bookId)
+        {
+            var chapters = _chapterRepository.GetChapters(bookId);
+            return Ok(chapters);
+        }
+    }
+}
---
+++ server/PlayLingua.WebApi/Controllers/ChaptersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayLingua.Domain.Entities;
+using PlayLingua.Domain.Ports;
+using System;
+using System.Collections.Generic;
+
+namespace PlayLingua.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChaptersController : ControllerBase
+    {
+        private readonly IWordRepository _wordRepository;
+
+        public ChaptersController(IWordRepository wordRepository)
+        {
+            _wordRepository = wordRepository;
+        }
+
+        [HttpGet("{chapterId}/words")]
+        public ActionResult<List<Word>> Words(Guid chapterId)
+        {
+            var words = _wordRepository.GetWords(chapterId);
+            return Ok(words);
+        }
+    }
+}
