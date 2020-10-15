0ffcd3d4bc778e344bb4f2d9bfdd93ce2930ca23
unknown <parissa.parsania@gmail.com>
2020-10-15 14:46:32 +0200

    changes in main files for adding the welcome page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DashboardComponent} from './modules/main/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-  ],
-  imports: [
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    FontAwesomeModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserAnimationsModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/modules/main/main.module.ts
+++ client/src/app/modules/main/main.module.ts
@@ -1,24 +1,32 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MainComponent} from './main.component';
-import {MainRoutingModule} from './main-routing.module';
-import {FallingStarsComponent} from './game/falling-stars/falling-stars.component';
-import {FormsModule} from '@angular/forms';
-import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+import { MainRoutingModule } from './main-routing.module';
+import { FallingStarsComponent } from './game/falling-stars/falling-stars.component';
+import { FormsModule } from '@angular/forms';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { SuperMarioComponent } from './game/super-mario/super-mario.component';
-import {MaterialModule} from '../common/material/material.module';
-
+import { MaterialModule } from '../common/material/material.module';
 
 @NgModule({
-  declarations: [MainComponent, FallingStarsComponent, SuperMarioComponent],
+  declarations: [
+    MainComponent,
+    FallingStarsComponent,
+    SuperMarioComponent,
+    DashboardComponent,
+  ],
   imports: [
     CommonModule,
     MainRoutingModule,
     FormsModule,
     FontAwesomeModule,
-    MaterialModule
-  ]
+    MaterialModule,
+  ],
 })
 export class MainModule {
   constructor(private iconLibrary: FaIconLibrary) {
