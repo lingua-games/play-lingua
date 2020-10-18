bbcd53961edc708fce39bd89b18ef7fcdc31dc7c
Senem Unal <senem@omniaretail.com>
2020-10-18 14:53:12 +0200

    Add description to LangCodeEnum

--- server/PlayLingua.Domain/LangCodeEnum.cs
+++ server/PlayLingua.Domain/LangCodeEnum.cs
@@ -1,9 +1,14 @@
-﻿namespace PlayLingua.Domain
+﻿using System.ComponentModel;
+
+namespace PlayLingua.Domain
 {
     public enum LangCodeEnum
     {
+        [Description("None")]
         None,
+        [Description("English")]
         EN,
+        [Description("Dutch")]
         NL
     }
 }
