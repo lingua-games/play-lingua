a0ad0be58244b86fd439852a0021a4b75a830d65
unknown <parissa.parsania@gmail.com>
2020-10-22 11:26:27 +0200

    GameMenu.model designed and initialized in dashboard component

---
+++ client/src/app/core/models/game.menu.model.ts
@@ -0,0 +1,3 @@
+export class GameMenu {
+  name: string;
+}
--- client/src/app/modules/main/dashboard/dashboard.component.ts
+++ client/src/app/modules/main/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
+import { GameMenu } from './../../../core/models/game.menu.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  gameMenus: GameMenu[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    this.gameMenus.push({ name: 'Super Mario' });
+    this.gameMenus.push({ name: 'Falling Star' });
+    this.gameMenus.push({ name: 'Game' });
+    this.gameMenus.push({ name: 'Game' });
+    this.gameMenus.push({ name: 'Game' });
+    this.gameMenus.push({ name: 'Game' });
   }
-
 }
