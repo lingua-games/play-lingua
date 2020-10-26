5942df6c1d3be10485873c67ee41d0327f9fb01b
unknown <vbhost.ir@gmail.com>
2020-10-26 08:55:31 +0100

    Wrote unit tests for super mario component

--- client/src/app/modules/main/game/super-mario/super-mario.component.spec.ts
+++ client/src/app/modules/main/game/super-mario/super-mario.component.spec.ts
@@ -23,4 +23,61 @@ describe('SuperMarioComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Key down events', () => {
+    it('startMovingLeft should be called on pressing arrow left', () => {
+      const mockEvent = { code: 'ArrowLeft' } as KeyboardEvent;
+      spyOn(component, 'startMovingLeft');
+
+      component.keyDownEvent(mockEvent);
+
+      expect(component.startMovingLeft).toHaveBeenCalled();
+    });
+
+    it('startMovingRight should be called on pressing arrow right', () => {
+      const mockEvent = { code: 'ArrowRight' } as KeyboardEvent;
+      spyOn(component, 'startMovingRight');
+
+      component.keyDownEvent(mockEvent);
+
+      expect(component.startMovingRight).toHaveBeenCalled();
+    });
+
+    it('jump should be called on pressing space key', () => {
+      const mockEvent = { code: 'Space' } as KeyboardEvent;
+      spyOn(component, 'jump');
+
+      component.keyDownEvent(mockEvent);
+
+      expect(component.jump).toHaveBeenCalled();
+    });
+  });
+
+  describe('Key up events', () => {
+    it('startMovingLeft should be called on pressing arrow left', () => {
+      const mockEvent = { code: 'ArrowLeft' } as KeyboardEvent;
+      spyOn(component, 'startMovingLeft');
+
+      component.keyDownEvent(mockEvent);
+
+      expect(component.startMovingLeft).toHaveBeenCalled();
+    });
+
+    it('startMovingRight should be called on pressing arrow right', () => {
+      const mockEvent = { code: 'ArrowRight' } as KeyboardEvent;
+      spyOn(component, 'startMovingRight');
+
+      component.keyDownEvent(mockEvent);
+
+      expect(component.startMovingRight).toHaveBeenCalled();
+    });
+  });
+
+  it('getWords should be called in startGame method', () => {
+    spyOn(component, 'getWords');
+
+    component.startGame();
+
+    expect(component.getWords).toHaveBeenCalled();
+  });
 });
