5f728109752103f29e0517a8345f4da6ab45e8be
unknown <parissa.parsania@gmail.com>
2020-10-30 19:05:19 +0100

    end 2 end test for welcome page

--- client/e2e/src/welcome.e2e-spec.ts
+++ client/e2e/src/welcome.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { AppPage } from './app.po';
 import { browser, by, element, logging } from 'protractor';
+import { url } from 'inspector';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
+    // arrange part for all of the tests below
     page = new AppPage();
     page.navigateTo();
   });
@@ -33,5 +35,37 @@ describe('workspace-project App', () => {
 
     // Assert
     expect(expectedText).toEqual('Play & Learn a New Language!');
+  });
+
+  it("should present Let's Get Started! button", () => {
+    // arrange: before Each()
+
+    // act
+    const expectedText = element(by.css('.start-button')).getText();
+
+    // assert
+    expect(expectedText).toEqual("Let's Get Started!");
+  });
+
+  it("should open the games-menu page when clicked on Let's Get Started! button", () => {
+    //act
+    element(by.css('.start-button')).click();
+    const expectedURL = browser.getCurrentUrl();
+
+    // assert
+    expect(expectedURL).toEqual('http://localhost:4200/main');
+  });
+
+  it('should present Admin button', () => {
+    const expectedText = element(by.css('.admin-button')).getText();
+    expect(expectedText).toEqual('Admin');
+  });
+
+  it('should present a background picture', () => {
+    const expectedBackgroundImage = element(
+      by.css('.welcome-body')
+    ).getCssValue('background-image');
+
+    expect(expectedBackgroundImage).toContain('welcome-background.png');
   });
 });
