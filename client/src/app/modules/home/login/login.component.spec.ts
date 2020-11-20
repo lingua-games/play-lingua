e72d06a07a364934b70bb5073981b80530aca549
Arash <vbhost.ir@gmail.com>
2020-11-20 17:26:57 +0100

    Fix failed unit tests

--- client/src/app/modules/home/login/login.component.spec.ts
+++ client/src/app/modules/home/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [LoginComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
