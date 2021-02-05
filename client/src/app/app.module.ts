7cad4ed08fd4d2fd3342e5eded2a689aa8e46938
Arash <vbhost.ir@gmail.com>
2021-02-05 15:53:43 +0100

    Added NGRX package

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorIntercept } from './core/interceptors/error.interceptor';
 import { TokenIntercept } from './core/interceptors/token.interceptor';
 import { MessageService } from 'primeng/api';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +24,7 @@ import { MessageService } from 'primeng/api';
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
   ],
   providers: [
     MessageService,
