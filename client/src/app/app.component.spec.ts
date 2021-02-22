4dcfae7c6eb291e47678fae79595db8d1952759e
Arash <vbhost.ir@gmail.com>
2021-02-22 11:50:26 +0100

    Fixed failover unit tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -2,12 +2,20 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [AppComponent],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+      ],
       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
