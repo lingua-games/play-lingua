e8ac8578230fb85d2e19d4d7e0824384cd3dfa1b
Arash <vbhost.ir@gmail.com>
2021-02-23 11:10:22 +0100

    Generalized finish and start components into a separate directory

--- client/src/app/modules/games/falling-stars/falling-stars.module.ts
+++ client/src/app/modules/games/falling-stars/falling-stars.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FallingStarsComponent } from './falling-stars.component';
 import { FallingStarsRoutingModule } from './falling-stars-routing.module';
 import { FormsModule } from '@angular/forms';
-import { StartGameDialogComponent } from './start-game-dialog/start-game-dialog.component';
 import { MaterialModule } from '../../common/material/material.module';
 import { ComponentModule } from '../../../core/component/component.module';
 import { StoreModule } from '@ngrx/store';
 import { scoreNotificationReducer } from '../../../core/component/score-notification/state/score-notification.reducer';
-import { FinishGameDialogComponent } from './finish-game-dialog/finish-game-dialog.component';
-import { TopNavbarComponent } from '../../home/top-navbar/top-navbar.component';
+import { StartGameDialogComponent } from '../common-in-game/start-game-dialog/start-game-dialog.component';
+import { FinishGameDialogComponent } from '../common-in-game/finish-game-dialog/finish-game-dialog.component';
 
 @NgModule({
   declarations: [
