1ef1a2b12e6acd57666c6cb6edd0f0a52d932bdf
Arash <vbhost.ir@gmail.com>
2021-03-02 12:39:30 +0100

    Fixed failover unit tests for guards

--- client/src/app/core/service/guards/only-user-allow.service.spec.ts
+++ client/src/app/core/service/guards/only-user-allow.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OnlyUserAllowService } from './only-user-allow.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('OnlyUserAllowService', () => {
   let service: OnlyUserAllowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+      ],
+    });
     service = TestBed.inject(OnlyUserAllowService);
   });
 
--- client/src/app/core/service/guards/user-and-guest-allow.service.spec.ts
+++ client/src/app/core/service/guards/user-and-guest-allow.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserAndGuestAllowService } from './user-and-guest-allow.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('UserAndGuestAllowService', () => {
   let service: UserAndGuestAllowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+      ],
+    });
     service = TestBed.inject(UserAndGuestAllowService);
   });
 
