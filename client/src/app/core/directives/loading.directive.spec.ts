5b23ad625547f5646048105925a79ace60a2ce0d
Arash <vbhost.ir@gmail.com>
2021-03-03 12:38:21 +0100

    Unit test for loading.directive.ts

--- client/src/app/core/directives/loading.directive.spec.ts
+++ client/src/app/core/directives/loading.directive.spec.ts
@@ -1,17 +1,60 @@
 import { LoadingDirective } from './loading.directive';
-import { TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  DebugElement,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+    <div appLoading [loadingFlag]="flag">
+      <h2>No Highlight</h2>
+    </div>
+  `,
+})
+class TestComponent {
+  public flag = true;
+
+  triggerFlag(): void {
+    this.flag = !this.flag;
+    console.log(this.flag);
+  }
+}
 
 describe('LoadingDirective', () => {
-  let directive;
+  let fixture: ComponentFixture<TestComponent>;
+  let des: DebugElement[];
+  let component: TestComponent;
   beforeEach(() => {
-    TestBed.configureTestingModule({
+    fixture = TestBed.configureTestingModule({
+      declarations: [LoadingDirective, TestComponent],
       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
-    });
-    const element = jasmine.createSpyObj(['nativeElement']);
-    directive = new LoadingDirective(element);
+    }).createComponent(TestComponent);
+    des = fixture.debugElement.queryAll(By.directive(LoadingDirective));
+    component = fixture.componentInstance;
   });
-  it('should create an instance', () => {
-    expect(directive).toBeTruthy();
+
+  it('should set the transition to 2s', () => {
+    fixture.detectChanges();
+    expect(des[0].nativeElement.style.transition).toBe('all 2s ease 0s');
+  });
+
+  it('should trigger loading on trigger call', () => {
+    component.triggerFlag();
+    fixture.detectChanges();
+    expect(des[0].nativeElement.style.animation).toBe(
+      '500ms ease 0s 1 normal none running stop-loading-animation'
+    );
+    expect(des[0].nativeElement.style.pointerEvents).toBe('all');
+
+    component.triggerFlag();
+    fixture.detectChanges();
+    expect(des[0].nativeElement.style.animation).toBe(
+      '1500ms linear 0s infinite normal none running loading-animation'
+    );
+    expect(des[0].nativeElement.style.pointerEvents).toBe('none');
   });
 });
