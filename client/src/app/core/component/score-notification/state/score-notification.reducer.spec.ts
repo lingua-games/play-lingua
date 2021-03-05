e50016acf58480c4f666371b1ca94530f0d0b16a
Arash <vbhost.ir@gmail.com>
2021-03-05 17:56:23 +0100

    Exclude reducer from code-coverage

---
+++ client/src/app/core/component/score-notification/state/score-notification.reducer.spec.ts
@@ -0,0 +1,16 @@
+// import * as reducer from './score-notification.reducer';
+//
+// describe('getNotificationFeatureState', () => {
+//   let getShowNotification;
+//
+//   beforeEach(() => {
+//     getShowNotification = getShowNotification;
+//   });
+//   it('should pass notification as parameter', () => {
+//     spyOn(reducer, 'scoreNotificationReducer');
+//
+//     reducer.getShowNotification(reducer.getNotificationFeatureState);
+//
+//     expect(reducer.getShowNotification).toHaveBeenCalled();
+//   });
+// });
