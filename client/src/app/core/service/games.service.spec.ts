50a64401384226eac4a6d683dd0476d02312796e
Arash <vbhost.ir@gmail.com>
2021-03-05 12:19:20 +0100

    unit test for book-chapter.service.ts

--- client/src/app/core/service/games.service.spec.ts
+++ client/src/app/core/service/games.service.spec.ts
@@ -3,9 +3,17 @@ import { TestBed } from '@angular/core/testing';
 import { GamesService } from './games.service';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BookChapterService } from './book-chapter.service';
+import { AddWordFormModel } from '../models/add-word-form.model';
+import { BookModel } from '../models/book.model';
+import { ChapterModel } from '../models/chapter.model';
+import { GetGameWordsRequestModel } from '../models/get-game-words-request.model';
 
 describe('GamesService', () => {
   let service: GamesService;
+  let httpClientSpy: {
+    post: jasmine.Spy;
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +21,26 @@ describe('GamesService', () => {
       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
     });
     service = TestBed.inject(GamesService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    // tslint:disable-next-line:no-any
+    service = new GamesService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call getBooks API when getBooks() method hit', () => {
+    const expectedForm: GetGameWordsRequestModel = {
+      defaultBaseLanguage: 1,
+      count: 1,
+      chapterId: 1,
+      bookId: 1,
+      defaultTargetLanguage: 1,
+    };
+
+    service.getGameWords(expectedForm);
+
+    expect(httpClientSpy.post).toHaveBeenCalled();
   });
 });
