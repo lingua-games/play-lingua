bab238415fd70798443c43d46948a779e4084e4f
Arash <vbhost.ir@gmail.com>
2021-03-10 14:20:36 +0100

    Unit test for profile.component.ts file

--- client/src/app/modules/home/profile/profile.component.spec.ts
+++ client/src/app/modules/home/profile/profile.component.spec.ts
@@ -1,28 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileComponent } from './profile.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NotificationService } from '../../../core/service/notification.service';
+import {
+  NotificationService,
+  Severity,
+} from '../../../core/service/notification.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SecurityService } from '../../../core/service/security.service';
 import { SecurityTokenInterface } from '../../../core/models/security-token.interface';
+import { EditUserModel } from '../../../core/models/edit-user.model';
+import { UserService } from '../../../core/service/user.service';
+import { of, throwError } from 'rxjs';
+import { LoginResultModel } from '../../../core/models/login-result.model';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
   let mockNotificationService;
   let mockSecurityService;
+  let mockUserService;
   beforeEach(async(() => {
     mockNotificationService = jasmine.createSpyObj(['showMessage']);
+    mockUserService = jasmine.createSpyObj(['editUser']);
     mockSecurityService = jasmine.createSpyObj({
       getTokenInformation: {} as SecurityTokenInterface,
+      setToken: () => {},
     });
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [ProfileComponent],
       providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
         {
           provide: NotificationService,
           useValue: mockNotificationService,
@@ -48,5 +62,113 @@ describe('ProfileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('submit', () => {
+    it('should set displayName error if field is empty', () => {
+      component.submit();
+
+      expect(component.errors['displayName']).toBe(
+        'Display name is a required field'
+      );
+    });
+
+    it('should set currentPassword error if field is empty', () => {
+      component.user = {
+        isChangingPassword: true,
+        displayName: 'fakeDisplayName',
+      } as EditUserModel;
+
+      component.submit();
+
+      expect(component.errors['currentPassword']).toBe(
+        'Current password is empty'
+      );
+    });
+
+    it('should set newPassword error if field is empty', () => {
+      component.user = {
+        isChangingPassword: true,
+        displayName: 'fakeDisplayName',
+        currentPassword: 'fakeCurrentPassword',
+      } as EditUserModel;
+
+      component.submit();
+
+      expect(component.errors['newPassword']).toBe('New password is empty');
+    });
+
+    it('should set newPassword error if newPassword and re-newPassword are different', () => {
+      component.user = {
+        isChangingPassword: true,
+        displayName: 'fakeDisplayName',
+        currentPassword: 'fakeCurrentPassword',
+        newPassword: 'fake new password',
+        reNewPassword: 'fake re new password',
+      } as EditUserModel;
+
+      component.submit();
+
+      expect(component.errors['newPassword']).toBe(
+        'New password and Re-new password are not match'
+      );
+    });
+
+    it('should set new token after API success', () => {
+      component.user = {
+        isChangingPassword: true,
+        displayName: 'fakeDisplayName',
+        currentPassword: 'fakeCurrentPassword',
+        newPassword: 'fake new password',
+        reNewPassword: 'fake new password',
+      } as EditUserModel;
+      mockUserService.editUser.and.callFake(() => {
+        return of({ token: 'fakeToken' } as LoginResultModel);
+      });
+
+      component.submit();
+
+      expect(mockSecurityService.setToken).toHaveBeenCalledWith('fakeToken');
+    });
+
+    it('should show success message after API success', () => {
+      component.user = {
+        isChangingPassword: true,
+        displayName: 'fakeDisplayName',
+        currentPassword: 'fakeCurrentPassword',
+        newPassword: 'fake new password',
+        reNewPassword: 'fake new password',
+      } as EditUserModel;
+      mockUserService.editUser.and.callFake(() => {
+        return of({ token: 'fakeToken' } as LoginResultModel);
+      });
+
+      component.submit();
+
+      expect(mockNotificationService.showMessage).toHaveBeenCalledWith(
+        'Profile saved',
+        Severity.success
+      );
+    });
+
+    it('should show Error message after API fail', () => {
+      component.user = {
+        isChangingPassword: true,
+        displayName: 'fakeDisplayName',
+        currentPassword: 'fakeCurrentPassword',
+        newPassword: 'fake new password',
+        reNewPassword: 'fake new password',
+      } as EditUserModel;
+      mockUserService.editUser.and.callFake(() => {
+        return throwError('fake error');
+      });
+
+      component.submit();
+
+      expect(mockNotificationService.showMessage).toHaveBeenCalledWith(
+        'fake error',
+        Severity.error
+      );
+    });
   });
 });
