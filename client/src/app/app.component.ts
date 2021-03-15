b6151ffc5d0908e71759ce35b5bf02b5ea622a98
Arash <vbhost.ir@gmail.com>
2021-03-15 22:42:52 +0100

    test pre-commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { UserModel } from './core/models/user.model';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-})  
+})
 export class AppComponent implements OnInit {
   title = 'client';
 
