c49c9067e39757ebf97aeb3ad44be9e71551a82c
Arash <vbhost.ir@gmail.com>
2021-03-18 19:10:51 +0100

    Refactor LanguageController

---
+++ server/PlayLingua.Contract/ViewModels/LanguageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayLingua.Contract.ViewModels
+{
+    public class Language
+    {
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string NativeName { get; set; }
+        public string FullName { get; set; }
+    }
+}
