a063b9e3feec42f2d42c3cea6b92cdba1adabe65
Arash <vbhost.ir@gmail.com>
2021-03-29 16:45:09 +0200

    Design skip button for super mario game

--- client/src/app/core/component/exit-game/exit-game.component.ts
+++ client/src/app/core/component/exit-game/exit-game.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exit-game',
   templateUrl: './exit-game.component.html',
   styleUrls: ['./exit-game.component.scss'],
 })
-export class ExitGameComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ExitGameComponent {
+  @Input() hasSkip?: boolean;
 }
