d2c19dc3364088aee9dc2045389cca2d4c680acc
Arash <vbhost.ir@gmail.com>
2021-04-20 10:12:02 +0200

    Fixed front-end build issue

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '',
+  productionUrl: 'https://play-lingua-beta.azurewebsites.net/',
   intervalForRoundMainPage: 1500,
   startGameCount: 50,
   recordCount: 5,
