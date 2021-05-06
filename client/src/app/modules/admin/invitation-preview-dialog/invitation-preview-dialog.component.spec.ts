4e36a5b353e2af00dd35bdfc979526f170d5eaa0
Arash <vbhost.ir@gmail.com>
2021-05-06 14:50:13 +0200

    Fixed failover unit tests

--- client/src/app/modules/admin/invitation-preview-dialog/invitation-preview-dialog.component.spec.ts
+++ client/src/app/modules/admin/invitation-preview-dialog/invitation-preview-dialog.component.spec.ts
@@ -1,16 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InvitationPreviewDialogComponent } from './invitation-preview-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('InvitationPreviewDialogComponent', () => {
   let component: InvitationPreviewDialogComponent;
   let fixture: ComponentFixture<InvitationPreviewDialogComponent>;
+  let mockMatDialogRef;
 
   beforeEach(async () => {
+    mockMatDialogRef = jasmine.createSpyObj(['close']);
+
     await TestBed.configureTestingModule({
-      declarations: [ InvitationPreviewDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [InvitationPreviewDialogComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockMatDialogRef,
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {},
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
