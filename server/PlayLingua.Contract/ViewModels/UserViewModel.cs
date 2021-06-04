9526213bcab58eab5f53e4a21b48c3cfd15ff2a5
Arash <vbhost.ir@gmail.com>
2021-06-04 11:32:52 +0200

    Added isAdmin flag into the token at login time

--- server/PlayLingua.Contract/ViewModels/UserViewModel.cs
+++ server/PlayLingua.Contract/ViewModels/UserViewModel.cs
@@ -12,6 +12,7 @@ namespace PlayLingua.Contract.ViewModels
         public bool IsChangingPassword { get; set; }
         public float TotalScore { get; set; }
         public string EmailVerificationCode { get; set; }
+        public bool IsAdmin { get; set; }
         public bool IsEmailVerified { get; set; }
         public DateTime EmailVerifiedDate { get; set; }
 #nullable enable
