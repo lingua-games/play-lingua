f6bf27ac9d8cd5128b63a609d73e91aef37d56a3
Arash <vbhost.ir@gmail.com>
2021-08-03 16:47:28 +0200

    unit test for ranking.component.ts file

--- client/src/app/core/component/ranking/ranking.component.spec.ts
+++ client/src/app/core/component/ranking/ranking.component.spec.ts
@@ -10,18 +10,28 @@ import { MatDialog } from '@angular/material/dialog';
 import { GameInformationInterface } from '../../models/game-information.interface';
 import { ScoreStoreInterface } from '../../models/score-store.interface';
 import { InvitationForm } from '../../models/invitation-form.interface';
+import { LocalStorageService } from '../../service/local-storage.service';
+import { LocalStorageHelper } from '../../models/local-storage.enum';
+import { BookModel } from '../../models/book.model';
+import { ChapterModel } from '../../models/chapter.model';
+import { LanguageModel } from '../../models/language.model';
 
 describe('RankingComponent', () => {
   let component: RankingComponent;
   let fixture: ComponentFixture<RankingComponent>;
   let mockScoreStorageService;
   let mockMatDialogRef;
+  let mockLocalStorageService;
 
   beforeEach(async () => {
     mockMatDialogRef = jasmine.createSpyObj(['close']);
     mockScoreStorageService = jasmine.createSpyObj('scoreStorageService', {
       getTopRanks: of(),
       storeScore: of(),
+    });
+    mockLocalStorageService = jasmine.createSpyObj('localStorageService', {
+      load: '{}',
+      clear: () => {},
     });
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
@@ -34,6 +44,10 @@ describe('RankingComponent', () => {
         {
           provide: MatDialog,
           useValue: mockMatDialogRef,
+        },
+        {
+          provide: LocalStorageService,
+          useValue: mockLocalStorageService,
         },
       ],
       schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
@@ -50,74 +64,127 @@ describe('RankingComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should store ranks if game is finished on initial time', () => {
-    component.data = { isGameFinished: true } as GameInformationInterface;
-    spyOn(component, 'storeRanks');
+  describe('storeRanks', () => {
+    it('should call service with feedback information if its feedback session', () => {
+      component.data = {
+        isFeedback: true,
+        feedbackForm: {
+          book: {} as BookModel,
+          chapter: {} as ChapterModel,
+          baseLanguage: {} as LanguageModel,
+          targetLanguage: {} as LanguageModel,
+        } as InvitationForm,
+      } as GameInformationInterface;
 
-    fixture.detectChanges();
+      component.storeRanks();
 
-    expect(component.storeRanks).toHaveBeenCalled();
-  });
+      expect(mockScoreStorageService.storeScore).toHaveBeenCalledWith({
+        feedbackUniqueKey: undefined,
+        isFeedback: true,
+        email: undefined,
+        bookId: undefined,
+        chapterId: undefined,
+        gameName: undefined,
+        baseLanguageId: undefined,
+        targetLanguageId: undefined,
+        count: 5,
+      });
+    });
 
-  it('should call getRanks on initial time', () => {
-    spyOn(component, 'getRanks');
+    it('should store ranks if game is finished on initial time', () => {
+      component.data = { isGameFinished: true } as GameInformationInterface;
+      spyOn(component, 'storeRanks');
 
-    fixture.detectChanges();
+      fixture.detectChanges();
 
-    expect(component.getRanks).toHaveBeenCalled();
-  });
+      expect(component.storeRanks).toHaveBeenCalled();
+    });
 
-  it('should stop loading after calling storeScore', () => {
-    mockScoreStorageService.storeScore.and.callFake(() => {
-      return of([
-        {
-          email: 'email 1',
+    it('should stop loading after calling storeScore', () => {
+      mockScoreStorageService.storeScore.and.callFake(() => {
+        return of([
+          {
+            email: 'email 1',
+            score: 1,
+            displayName: 'displayName 1',
+          } as RanksResultInterface,
+          {
+            email: 'email 2',
+            score: 2,
+            displayName: 'displayName 2',
+          } as RanksResultInterface,
+        ]);
+      });
+      component.data = {
+        feedbackForm: { uniqueKey: 'fake key' } as InvitationForm,
+        scoreStore: {
           score: 1,
-          displayName: 'displayName 1',
-        } as RanksResultInterface,
-        {
-          email: 'email 2',
-          score: 2,
-          displayName: 'displayName 2',
-        } as RanksResultInterface,
-      ]);
+        } as ScoreStoreInterface,
+      } as GameInformationInterface;
+
+      component.storeRanks();
+
+      expect(component.ranks.isLoading).toBeFalsy();
     });
-    component.data = {
-      feedbackForm: { uniqueKey: 'fake key' } as InvitationForm,
-      scoreStore: {
-        score: 1,
-      } as ScoreStoreInterface,
-    } as GameInformationInterface;
 
-    component.storeRanks();
+    it('should stop loading if storeScore fail', () => {
+      mockScoreStorageService.storeScore.and.callFake(() => {
+        return throwError('error message');
+      });
+      component.data = {
+        feedbackForm: { uniqueKey: 'fake key' } as InvitationForm,
+        scoreStore: {
+          score: 1,
+        } as ScoreStoreInterface,
+      } as GameInformationInterface;
 
-    expect(component.ranks.isLoading).toBeFalsy();
+      component.storeRanks();
+
+      expect(component.ranks.isLoading).toBeFalsy();
+    });
   });
 
-  it('should stop loading if storeScore fail', () => {
-    mockScoreStorageService.storeScore.and.callFake(() => {
-      return throwError('error message');
-    });
-    component.data = {
-      feedbackForm: { uniqueKey: 'fake key' } as InvitationForm,
-      scoreStore: {
-        score: 1,
-      } as ScoreStoreInterface,
-    } as GameInformationInterface;
+  describe('getRanks', () => {
+    it('should call getRanks on initial time', () => {
+      spyOn(component, 'getRanks');
 
-    component.storeRanks();
+      fixture.detectChanges();
 
-    expect(component.ranks.isLoading).toBeFalsy();
-  });
-
-  it('should set Nobody played yet as error if there is no rank for this session', () => {
-    mockScoreStorageService.getTopRanks.and.callFake(() => {
-      return of([]);
+      expect(component.getRanks).toHaveBeenCalled();
     });
 
-    component.getRanks();
+    it('should set Nobody played yet as error if there is no rank for this session', () => {
+      mockScoreStorageService.getTopRanks.and.callFake(() => {
+        return of([]);
+      });
 
-    expect(component.ranks.errorMessage).toBe('Nobody played yet.');
+      component.getRanks();
+
+      expect(component.ranks.errorMessage).toBe('Nobody played yet.');
+    });
+
+    it('should call scoreStorageService.getTopRanks with feedback information if its feedback session', () => {
+      component.data = {
+        isFeedback: true,
+        feedbackForm: {
+          book: {} as BookModel,
+          chapter: {} as ChapterModel,
+          baseLanguage: {} as LanguageModel,
+          targetLanguage: {} as LanguageModel,
+        } as InvitationForm,
+      } as GameInformationInterface;
+
+      component.getRanks();
+
+      expect(mockScoreStorageService.getTopRanks).toHaveBeenCalledWith({
+        bookId: undefined,
+        chapterId: undefined,
+        count: 10,
+        gameName: undefined,
+        baseLanguageId: undefined,
+        targetLanguageId: undefined,
+      });
+    });
   });
 
   it('should set and sort ranks into this.ranks when API fetch ranks', () => {
@@ -156,5 +223,17 @@ describe('RankingComponent', () => {
     expect(component.ranks.errorMessage).toEqual(
       'Unable to get ranks, please try again later'
     );
+  });
+
+  describe('ngOnChanges', () => {
+    it('should call localStorageService.load if its not feedback session', () => {
+      component.data = { isFeedback: false } as GameInformationInterface;
+
+      component.ngOnChanges();
+
+      expect(mockLocalStorageService.load).toHaveBeenCalledWith(
+        LocalStorageHelper.defaultLanguages
+      );
+    });
   });
 });
