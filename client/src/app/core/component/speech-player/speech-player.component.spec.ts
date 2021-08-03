b629c7e3250fc798342e9fe6b07209813b27d1f2
Arash <vbhost.ir@gmail.com>
2021-08-04 00:47:26 +0200

    unit test for speech-player.component.ts file

--- client/src/app/core/component/speech-player/speech-player.component.spec.ts
+++ client/src/app/core/component/speech-player/speech-player.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpeechPlayerComponent } from './speech-player.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { SpeechStatus } from '../../models/word-key-value.model';
+import { global } from '@angular/compiler/src/util';
 
 describe('SpeechPlayerComponent', () => {
   let component: SpeechPlayerComponent;
@@ -22,5 +24,20 @@ describe('SpeechPlayerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('playSound', () => {
+    it('should break if status is Error', () => {
+      component.status = SpeechStatus.Error;
+
+      expect(component.playSound()).toBe(undefined);
+    });
+
+    xit('should break if status is Error', () => {
+      component.status = SpeechStatus.Success;
+
+      component.playSound();
+      expect(undefined).toBe(undefined);
+    });
   });
 });
